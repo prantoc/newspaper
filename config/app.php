1e8f817cbdf56498b05412d1224c85d6035eb19d
Pranto <chakrabortypranto39@gmail.com>
2021-11-22 14:14:32 +0600

    foe asia timezone

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Dhaka',
 
     /*
     |--------------------------------------------------------------------------
@@ -166,6 +166,9 @@ return [
          * Package Service Providers...
          */
 
+        Jorenvh\Share\Providers\ShareServiceProvider::class,
+
+
         /*
          * Application Service Providers...
          */
@@ -228,6 +231,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+
+        'Share' => Jorenvh\Share\ShareFacade::class,
 
     ],
 
